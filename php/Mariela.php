a23d557b9d99ad9d61e895c7b21fba5671bcb226
Mariela Peña Robles <Marieladisenosgraficos@gmail.com>
2026-03-04 22:41:25 -0400

    actualizacion

--- php/Mariela.php
+++ php/Mariela.php
@@ -1,4 +1,18 @@
 <?php
+if(!empty($_POST['btn_entrar'])) {
+    if(empty($_POST['usuario']) && empty($_POST['contrasena'])) {
+        echo '<div class="alert alert-danger" role="alert>Los Campos Estan Vacios</div>';
+    } else {
+        $usuario=$_POST['usuario'];
+        $password=$_POST['contrasena'];
+        $sql=$conexion->query("select * from usuario where usuario='$usuario' and password='$password' ");
+        if($datos=$sql ->fetch:object()) {
+            header("location:inicio.php");
+        } else {
+            echo '<div class="alert alert-danger" role="alert>ACCESO DENEGADO!!</div>';
+        }
+    }
 
+}
 
 ?>
