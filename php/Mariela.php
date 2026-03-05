ba5b7cd9b77ccf13fbc4a66486e08f74051b4b90
Mariela Peña Robles <Marieladisenosgraficos@gmail.com>
2026-03-04 23:23:36 -0400

    actualizacion

--- php/Mariela.php
+++ php/Mariela.php
@@ -1,15 +1,17 @@
 <?php
 if(!empty($_POST['btn_entrar'])) {
     if(empty($_POST['usuario']) && empty($_POST['contrasena'])) {
-        echo '<div class="alert alert-danger" role="alert>Los Campos Estan Vacios</div>';
+        echo '<div class="alert alert-danger" role="alert">Los Campos Estan Vacios</div>';
     } else {
         $usuario=$_POST['usuario'];
         $password=$_POST['contrasena'];
+        $usuario = htmlspecialchars($usuario);
+        $password = htmlspecialchars($password);
         $sql=$conexion->query("select * from usuario where usuario='$usuario' and password='$password' ");
-        if($datos=$sql ->fetch:object()) {
-            header("location:inicio.php");
+        if($datos=$sql->fetch_object()) {
+            header("location:inicio.php"); //aqui va la pagina despues del login
         } else {
-            echo '<div class="alert alert-danger" role="alert>ACCESO DENEGADO!!</div>';
+            echo '<div class="alert alert-danger" role="alert">ACCESO DENEGADO!!</div>';
         }
     }
 
