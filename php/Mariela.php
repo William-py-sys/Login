7454060621fb7ee83b10e96846b57602300fc4c3
Mariela Peña Robles <Marieladisenosgraficos@gmail.com>
2026-03-05 17:03:18 -0400

    mensaje

--- php/Mariela.php
+++ php/Mariela.php
@@ -1,34 +1,31 @@
 <?php
 include_once('../base_de_datos/conexion.php');
 
-if (!empty($_POST['btn_entrar'])) {
+if (empty($_POST['usuario']) || empty($_POST['contrasena'])) {
 
-    if (empty($_POST['usuario']) || empty($_POST['contrasena'])) {
+    echo '<div class="alert alert-danger" role="alert">Los campos están vacíos</div>';
 
-        echo '<div class="alert alert-danger" role="alert">Los campos están vacíos</div>';
+} else {
 
-    } else {
+    $usuario = trim($_POST['usuario']);
+    $password = trim($_POST['contrasena']);
 
-        $usuario = trim($_POST['usuario']);
-        $password = trim($_POST['contrasena']);
+    $sql = $conexion->query("SELECT * FROM usuarios WHERE usuario='$usuario'");
 
-        $sql = $conexion->query("SELECT * FROM usuarios WHERE usuario='$usuario'");
+    if ($datos = $sql->fetch_object()) {
 
-        if ($datos = $sql->fetch_object()) {
-
-            if ($password == $datos->password) {
-                header("Location: inicioindex.php");
-                exit();
-
-            } else {
-
-                echo '<div class="alert alert-danger" role="alert">Contraseña incorrecta</div>';
-            }
+        if ($password == $datos->password) {
+            header("Location: inicioindex.php");
+            exit();
 
         } else {
 
-            echo '<div class="alert alert-danger" role="alert">Usuario no encontrado</div>';
+            echo '<div class="alert alert-danger" role="alert">Contraseña incorrecta</div>';
         }
+
+    } else {
+
+        echo '<div class="alert alert-danger" role="alert">Usuario no encontrado</div>';
     }
 }
 ?>
