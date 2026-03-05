65c1a1653590db520eeabe587ef4422087962ad1
Mariela Peña Robles <Marieladisenosgraficos@gmail.com>
2026-03-05 17:52:06 -0400

    seguro

--- php/loginindex.php
+++ php/loginindex.php
@@ -1,5 +1,4 @@
 <?php
-print_r($_POST);
 include_once("configuracion.php");
 
 // Link conexion con variables
@@ -179,6 +178,3 @@ a{
         <strong>  © <?php echo date("Y"); ?> Programación I</strong>
     </footer>
     
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</body>
-</html>
